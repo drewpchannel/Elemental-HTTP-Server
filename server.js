97b3ccfc59a5925f9706a4907f797af97855961b
dpcc <drewpcchannel@gmail.com>
2016-07-08 18:23:45 -1000

    sorting requests

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const http = require ('http');
 const fs = require ('fs');
 
 const server = http.createServer((req, res) => {
-  server.on('data', (data) => {console.log(data.toString())});
+  console.log('Server created...');
   res.writeHead(200);
-  res.end('<h1> is this working? </h1>');
+  directReq(req, res);
 });
 
-server.listen(8080);
+server.listen(8080, () => {
+  console.log('Server listening...');
+});
+
+function directReq (req, res) {
+  if (req.method === 'GET') {
+    console.log(fs.readFile('./public/hydrogen.html'));
+  }
+}
